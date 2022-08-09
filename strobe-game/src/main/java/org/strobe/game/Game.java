5ebe51a6ee1448534b1978f6e5a4a5e485bda010
kistenklaus <karlsasssie@gmail.com>
2022-08-09 18:43:18 +0200

    fix for linux

--- strobe-game/src/main/java/org/strobe/game/Game.java
+++ strobe-game/src/main/java/org/strobe/game/Game.java
@@ -75,7 +75,6 @@ public class Game extends EntityContext {
 
 
 
-        /*
         Entity light2 = ecs.createEntity("light2");
         light2.addComponent(new Transform(new Vector3f(1,2,-2)));
         dirLightComp = new DirectionalLight(new Vector3f(0.1f), new Vector3f(1), new Vector3f(1));
@@ -87,7 +86,6 @@ public class Game extends EntityContext {
         dirLightComp = new DirectionalLight(new Vector3f(0.1f), new Vector3f(1), new Vector3f(1));
         dirLightComp.setShadowFrustumOffset(10);
         light3.addComponent(dirLightComp);
-         */
 
         Entity camera2 = ecs.createEntity("camera2");
         camera2.addComponent(new Transform(new Vector3f(0,0,5)));
